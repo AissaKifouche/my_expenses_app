88d5763bed41725998c8bab21d388edb59584a57
AissaKifouche <a_kifouche@estin.dz>
2026-08-31 19:55:27 +0100

    started home page

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:my_expenses/main_shell.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,9 +11,13 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'My Expenses',
-      home: MainShell(),
+    return ScreenUtilInit(
+      designSize: Size(412, 917),
+      minTextAdapt: true,
+      child: MaterialApp(
+        title: 'My Expenses',
+        home: MainShell(),
+      ),
     );
   }
 }
--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:my_expenses/buttom_clipper.dart';
+
+
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
@@ -10,7 +14,59 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text("home"),),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Stack(
+            children: [
+              Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  ClipPath(
+                    clipper: BottomCurveClipper(),
+                    child: Container(
+                      height: 260.h,
+                      width: double.infinity,
+                      padding: EdgeInsets.all(20),
+                      decoration: BoxDecoration(
+                        //shape: BoxShape.circle,
+                        gradient: LinearGradient(
+                          begin: Alignment.topLeft,
+                          end: Alignment.bottomRight,
+                          colors: [
+                            Color(0xFF429690),
+                            Color(0xFF058E84),
+                          ]
+                        )
+                      ),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          SizedBox(height: 20.h,),
+                          Text(
+                            'Good morning',
+                            style: TextStyle(
+                              fontSize: 16.sp,
+                              color: Colors.white,
+                            ),
+                          ),
+                          SizedBox(height: 5.h,),
+                          Text(
+                            'Dexter Morgan',
+                            style: TextStyle(
+                              fontSize: 22.sp,
+                              color: Colors.white,
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
