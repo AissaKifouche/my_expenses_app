fb6eedceb1ec9839f8e89d728b21af8753e9bcd1
AissaKifouche <a_kifouche@estin.dz>
2026-09-01 18:26:03 +0100

    little things to home page

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -26,129 +26,132 @@ class _HomePageState extends State<HomePage> {
       ),
       child: Scaffold(
         body: Stack(
-          children: [SingleChildScrollView(
-            child: Stack(
-              children: [
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
+          children: [
+            SingleChildScrollView(
+              child: Stack(
+                children: [
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
 
-                    //the top green container
-                    ClipPath(
-                      clipper: BottomCurveClipper(),
-                      child: Container(
-                        height: 300.h,
-                        width: double.infinity,
-                        padding: EdgeInsets.symmetric(horizontal:  20.w, vertical: 35.h),
-                        decoration: BoxDecoration(
-                          //shape: BoxShape.circle,
-                          gradient: LinearGradient(
-                            begin: Alignment.topLeft,
-                            end: Alignment.bottomRight,
-                            colors: [
-                              Color(0xFF429690),
-                              Color(0xFF058E84),
-                            ]
-                          )
+                      //the top green container
+                      ClipPath(
+                        clipper: BottomCurveClipper(),
+                        child: Container(
+                          height: 300.h,
+                          width: double.infinity,
+                          padding: EdgeInsets.symmetric(horizontal:  20.w, vertical: 35.h),
+                          decoration: BoxDecoration(
+                            //shape: BoxShape.circle,
+                            gradient: LinearGradient(
+                              begin: Alignment.topLeft,
+                              end: Alignment.bottomRight,
+                              colors: [
+                                Color(0xFF429690),
+                                Color(0xFF058E84),
+                              ]
+                            )
+                          ),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              SizedBox(height: 20.h,),
+                              Text(
+                                'Good morning',
+                                style: TextStyle(
+                                  fontSize: 16.sp,
+                                  color: Colors.white,
+                                ),
+                              ),
+                              SizedBox(height: 5.h,),
+                              Text(
+                                'Dexter Morgan',
+                                style: TextStyle(
+                                  fontSize: 22.sp,
+                                  color: Colors.white,
+                                ),
+                              )
+                            ],
+                          ),
                         ),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
+                      ),
+
+                      // a white space to show the budget card and leave a space between it and other elements
+                      SizedBox(height: 70.h,),
+
+                      //row of Recent transactions
+                      Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 20.w),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
                           children: [
-                            SizedBox(height: 20.h,),
                             Text(
-                              'Good morning',
+                              "Recent Transactions",
                               style: TextStyle(
-                                fontSize: 16.sp,
-                                color: Colors.white,
+                                fontSize: 20.sp,
+                                color: Colors.black,
+                                fontWeight: FontWeight.w500
                               ),
                             ),
-                            SizedBox(height: 5.h,),
-                            Text(
-                              'Dexter Morgan',
-                              style: TextStyle(
-                                fontSize: 22.sp,
-                                color: Colors.white,
+
+                            TextButton(
+                              onPressed: (){
+                                widget.onNavigateToTab(1);
+                              },
+                              child: Text(
+                                "See all",
+                                style: TextStyle(
+                                  color: Colors.grey,
+                                  fontSize: 14.sp
+                                ),
                               ),
-                            )
+                            ),
                           ],
                         ),
                       ),
-                    ),
 
-                    // a white space to show the budget card and leave a space between it and other elements
-                    SizedBox(height: 70.h,),
+                      SizedBox(height: 25.h,),
 
-                    //row of Recent transactions
-                    Padding(
-                      padding: EdgeInsets.symmetric(horizontal: 20.w),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text(
-                            "Recent Transactions",
-                            style: TextStyle(
-                              fontSize: 20.sp,
-                              color: Colors.black,
-                              fontWeight: FontWeight.w500
-                            ),
-                          ),
+                      //recent transactions
+                      TransactionCard(),
+                      SizedBox(height: 15.h,),
+                      TransactionCard(),
+                      SizedBox(height: 15.h,),
+                      TransactionCard(),
+                      SizedBox(height: 15.h,),
+                      TransactionCard(),
+                      SizedBox(height: 15.h,),
+                      TransactionCard(),
+                      SizedBox(height: 15.h,),
+                      TransactionCard(),
+                      SizedBox(height: 15.h,),
+                      TransactionCard(),
+                      SizedBox(height: 15.h,),
+                      TransactionCard(),
+                      SizedBox(height: 15.h,),
+                      TransactionCard(),
+                      SizedBox(height: 15.h,),
+                      TransactionCard(),
+                      SizedBox(height: 15.h,),
+                      TransactionCard(),
+                      SizedBox(height: 15.h,),
 
-                          TextButton(
-                            onPressed: (){
-                              widget.onNavigateToTab(1);
-                            },
-                            child: Text(
-                              "See all",
-                              style: TextStyle(
-                                color: Colors.grey,
-                                fontSize: 14.sp
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-
-                    SizedBox(height: 25.h,),
-
-                    //recent transactions
-                    TransactionCard(),
-                    SizedBox(height: 15.h,),
-                    TransactionCard(),
-                    SizedBox(height: 15.h,),
-                    TransactionCard(),
-                    SizedBox(height: 15.h,),
-                    TransactionCard(),
-                    SizedBox(height: 15.h,),
-                    TransactionCard(),
-                    SizedBox(height: 15.h,),TransactionCard(),
-                    SizedBox(height: 15.h,),
-                    TransactionCard(),
-                    SizedBox(height: 15.h,),
-                    TransactionCard(),
-                    SizedBox(height: 15.h,),
-                    TransactionCard(),
-                    SizedBox(height: 15.h,),
-                    TransactionCard(),
-                    SizedBox(height: 15.h,),
-                    TransactionCard(),
-                    SizedBox(height: 15.h,),
-
-                  ],
-                ),
-                Positioned(
-                  top: 120.h,
-                  left: 0.w,
-                  right: 0.w,
-                  child: Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 20),
-                    child: BudgetCard(),
+                    ],
                   ),
-                ),
-              ],
+                  Positioned(
+                    top: 120.h,
+                    left: 0.w,
+                    right: 0.w,
+                    child: Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 20),
+                      child: BudgetCard(),
+                    ),
+                  ),
+                ],
+              ),
             ),
-          ),
 
+            //status bar's background color
             Container(
               height: statusBarHeight,
               width: double.infinity,
