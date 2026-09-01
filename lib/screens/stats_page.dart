b2a32927d3c8ea0ed0a394f66b0b66d13fef5831
AissaKifouche <a_kifouche@estin.dz>
2026-09-01 22:56:39 +0100

    started stats page, done with app bar and TimeFilter buttond

--- lib/screens/stats_page.dart
+++ lib/screens/stats_page.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+
+enum TimeFilter {day, week, month, year}
+
 
 class StatsPage extends StatefulWidget {
   const StatsPage({super.key});
@@ -8,8 +13,68 @@ class StatsPage extends StatefulWidget {
 }
 
 class _StatsPageState extends State<StatsPage> {
+
+  TimeFilter selectedFilter = TimeFilter.day;
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color(0xFF219289),
+        title: Text(
+          "Statistics",
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 28.sp,
+          ),
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding:  EdgeInsets.symmetric(horizontal: 20.w, vertical: 10.h),
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: TimeFilter.values.map((filter) {
+                  final isSelected = selectedFilter == filter;
+
+                  return Expanded(
+                    child: Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 10.w), // Spacing between buttons
+                      child: InkWell(
+                        onTap: () => setState(() => selectedFilter = filter),
+                        borderRadius: BorderRadius.circular(12),
+                        child: AnimatedContainer(
+                          duration: Duration(milliseconds: 200),
+                          padding: EdgeInsets.symmetric(horizontal: 10.w, vertical: 10.h),
+                          decoration: BoxDecoration(
+                            color: isSelected ?  Color(0xFF438883) :  Colors.transparent, // Active vs Inactive card color
+                            borderRadius: BorderRadius.circular(12.r), // Individual rounded box
+                          ),
+                          child: Text(
+                            filter.name.capitalize(), // Displays Day, Week, Month, Year
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              color: isSelected ? Colors.white : Colors.grey.shade600,
+                              fontWeight: isSelected ? FontWeight.w600 : FontWeight.normal,
+
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  );
+                }).toList(),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
+}
+
+extension StringExtension on String {
+  String capitalize() => "${this[0].toUpperCase()}${substring(1)}";
 }
