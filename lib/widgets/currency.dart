e39ae2b476aa6da0a78d014b1659aea4a178507c
AissaKifouche <a_kifouche@estin.dz>
2026-09-05 20:33:20 +0100

    making statud bar icons dark

--- lib/widgets/currency.dart
+++ lib/widgets/currency.dart
@@ -47,7 +47,7 @@ class CurrencyContainerPicker extends StatelessWidget {
         border: Border.all(color: const Color(0xFFE0E0E0)),
         boxShadow: [
           BoxShadow(
-            color: Colors.black.withOpacity(0.04),
+            color: Colors.black.withValues(alpha: 0.04),
             blurRadius: 8,
             offset: const Offset(0, 2),
           ),
