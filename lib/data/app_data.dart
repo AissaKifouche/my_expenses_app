af9936fcfd2e4689bbe102407375658cbe7f977f
AissaKifouche <a_kifouche@estin.dz>
2026-09-12 13:35:04 +0100

    add addTransaction, deleteTransaction and editTransaction methods to AppData

--- lib/data/app_data.dart
+++ lib/data/app_data.dart
@@ -8,4 +8,25 @@ class AppData {
   );
 
   List<Transaction> transactions = [];
+
+  void addTransaction(Transaction transaction){
+    transactions.insert(0, transaction);
+    transaction.transactionType == TransactionType.income ? wallet.balance += transaction.amount : wallet.balance -= transaction.amount;
+  }
+
+  void deleteTransaction(String id){
+    int index = transactions.indexWhere((transaction) => transaction.id == id);
+    transactions[index].transactionType == TransactionType.income ?
+        wallet.balance -= transactions[index].amount
+        : wallet.balance += transactions[index].amount;
+    transactions.removeAt(index);
+  }
+
+  void editTransaction(Transaction updated){
+    int index = transactions.indexWhere((transaction) => transaction.id == updated.id);
+    if (index != -1){
+      transactions[index] = updated;
+    }
+  }
+
 }
