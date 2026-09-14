caa709d9aeca5a1dd2495abbb94e9b5b132b1e0d
AissaKifouche <a_kifouche@estin.dz>
2026-09-14 19:55:39 +0100

    delete files: day_stats week month year, because going to change what will appear on stats page and its filters

--- lib/screens/stats_page.dart
+++ lib/screens/stats_page.dart
@@ -1,10 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:my_expenses/data/app_data.dart';
-import 'package:my_expenses/widgets/day_stats.dart';
-import 'package:my_expenses/widgets/month_stats.dart';
-import 'package:my_expenses/widgets/week_stats.dart';
-import 'package:my_expenses/widgets/year_stats.dart';
+
 import 'package:flutter_utils/flutter_utils.dart';
 
 
@@ -23,18 +20,7 @@ class _StatsPageState extends State<StatsPage> {
 
   TimeFilter selectedFilter = TimeFilter.day;
 
-  Widget _buildFilteredLayout(){
-    switch(selectedFilter){
-      case TimeFilter.day :
-        return DayStats();
-      case TimeFilter.week:
-        return WeekStats();
-      case TimeFilter.month:
-        return MonthStats();
-      case TimeFilter.year:
-        return YearStats();
-    }
-  }
+
 
   @override
   Widget build(BuildContext context) {
@@ -54,63 +40,7 @@ class _StatsPageState extends State<StatsPage> {
           padding:  EdgeInsets.symmetric(horizontal: 20.w, vertical: 10.h),
           child: Column(
             children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: TimeFilter.values.map((filter) {
-                  final isSelected = selectedFilter == filter;
 
-                  return Expanded(
-                    child: Padding(
-                      padding: EdgeInsets.symmetric(horizontal: 10.w), // Spacing between buttons
-                      child: InkWell(
-                        onTap: () => setState(() => selectedFilter = filter),
-                        borderRadius: BorderRadius.circular(12),
-                        child: AnimatedContainer(
-                          duration: Duration(milliseconds: 200),
-                          padding: EdgeInsets.symmetric(horizontal: 10.w, vertical: 10.h),
-                          decoration: BoxDecoration(
-                            color: isSelected ?  Color(0xFF438883) :  Colors.transparent, // Active vs Inactive card color
-                            borderRadius: BorderRadius.circular(12.r), // Individual rounded box
-                          ),
-                          child: Text(
-                            filter.name.capitalize(), // Displays Day, Week, Month, Year
-                            textAlign: TextAlign.center,
-                            style: TextStyle(
-                              color: isSelected ? Colors.white : Colors.grey.shade600,
-                              fontWeight: isSelected ? FontWeight.w600 : FontWeight.normal,
-
-                            ),
-                          ),
-                        ),
-                      ),
-                    ),
-                  );
-                }).toList(),
-              ),
-
-              SizedBox(height: 30.h,),
-
-              _buildFilteredLayout(),
-              
-              TextButton(
-                onPressed: (){
-                  //TBD, loads more transactions
-                },
-                child: GradientText(
-                  "Load More",
-                  gradient: LinearGradient(
-                    begin: Alignment.topCenter,
-                    end: Alignment.bottomCenter,
-                    colors: [
-                      Colors.black,
-                      Colors.white
-                    ]
-                  ),
-                  style: TextStyle(
-                    fontSize: 16.sp
-                  ),
-                ),
-              ),
             ],
           ),
         ),
@@ -119,6 +49,3 @@ class _StatsPageState extends State<StatsPage> {
   }
 }
 
-extension StringExtension on String {
-  String capitalize() => "${this[0].toUpperCase()}${substring(1)}";
-}
